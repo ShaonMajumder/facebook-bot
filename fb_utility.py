dd71f1d3631e8afe21edb0becf2f5e9d189db020
Shaon Majumder <smazoomder@gmail.com>
2020-02-24 11:04:11 +0600

    diaglog oauth access token, getting permssions, permanent access token

---
+++ fb_utility.py
@@ -0,0 +1,98 @@
+from selenium import webdriver
+import shaonutil
+import requests
+import facebook
+
+def get_user_id(access_token):
+	API_URL = "https://graph.facebook.com/v3.0"
+	PARAMS = {'access_token':access_token}
+	r = requests.get(url = API_URL + '/me', params = PARAMS) 
+	data = r.json()
+	id_ = data['id']
+	return id_
+
+def debug_access_token(access_token):
+	url = 'https://graph.facebook.com/v3.2/debug_token'
+	PARAMS = {'input_token':access_token,'access_token':access_token}
+	r = requests.get(url = url, params = PARAMS)
+	data = r.json()
+	return data
+
+def getParams(url):
+    dict_ = {}
+    params = url.split("?")[1]
+    params = params.split('&')
+    for param in params:
+    	key, val = param.split('=',1)
+    	dict_[key] = val
+    # {'access_token': 'EAADKbVke6LMBAPfT8ZACpnmrTzS8GZAuLff7epYnfq6AmEExsVwjXr1inXqeC9fL6vQVzlLbRPOxxJinzAhnPg9RLAOWQZA0Go9ti0DvZAmndp3lZBzjg17cQUsTHggfhn0UpBmm85DLnqH7FZC2iZBZCizJc2c3Qo6fi2FzL3mfZCywvHFLERoMWG1HtmoERFxtemqSZCIISGHwZDZD', 'data_access_expiration_time': '1590265814', 'expires_in': '5386'}
+    return dict_
+
+def get_app_access_token(fb_app_id, fb_app_secret):
+    client = facebook.GraphAPI()
+    return client.get_app_access_token( fb_app_id, fb_app_secret)
+
+def get_access_token_via_dialogue(client_id,permissions):
+	redirect_uri = 'https://m.facebook.com'
+	driver = webdriver.Chrome('private/chromedriver.exe')
+	driver.get('https://www.facebook.com/dialog/oauth?client_id='+client_id+'&display=popup&response_type=token&redirect_uri='+redirect_uri+'&scope='+permissions)
+	while 'access_token' not in driver.current_url:
+		pass
+	
+	return_url = driver.current_url
+	return_url = return_url.replace('#','')
+	dict_ = getParams(return_url)
+	return dict_['access_token']
+
+
+
+def get_permanant_access_token(client_id,client_secret,access_token):
+	API_URL = "https://graph.facebook.com/v3.0"
+	PARAMS = {
+		'grant_type':'fb_exchange_token',
+		'client_id':client_id,
+		'client_secret':client_secret,
+		'fb_exchange_token':access_token
+	}
+
+	r = requests.get(url = API_URL + '/oauth/access_token', params = PARAMS) 
+	data = r.json()
+	# {'error': {'message': 'Error validating access token: Session has expired on Sunday, 23-Feb-20 08:00:00 PST. The current time is Sunday, 23-Feb-20 08:37:46 PST.', 'type': 'OAuthException', 'code': 190, 'error_subcode': 463, 'fbtrace_id': 'AZzY-xbsxuhYlmMUECWzuHV'}}
+	n_access_token = data['access_token']
+  
+
+	id_ = get_user_id(n_access_token)
+
+	PARAMS = {'access_token':n_access_token}
+
+	r = requests.get(url = API_URL + '/'+id_+'/accounts', params = PARAMS) 
+	data = r.json()
+	#print(data)
+	access_token = data['data'][0]['access_token']
+
+	debug_data = debug_access_token(access_token)
+	# {'error': {'message': 'Error validating access token: The session was invalidated previously using an API call.', 'type': 'OAuthException', 'code': 190, 'error_subcode': 463, 'fbtrace_id': 'AJR-Mef9z_ajsFlIK9IhtVf'}}
+	if debug_data['data']['expires_at'] == 0:
+		return access_token	
+	else:
+		raise ValueError("Getting permanent_user_access_token failed !")
+
+def routine_on_access_error_get_permanent_token(client_id,client_secret,permissions):
+	config = shaonutil.file.read_configuration_ini('config.ini')
+	
+
+	access_token = get_access_token_via_dialogue(client_id,permissions)
+	permanent_user_access_token = get_permanant_access_token(client_id,client_secret,access_token)
+
+	debug_data = debug_access_token(permanent_user_access_token)
+
+	runable = True
+	for pname in permissions.split(','):
+		if pname not in debug_data['data']['scopes']:
+			runable = False
+			raise ValueError("Getting all permissions failed")
+
+	if(runable):
+		config['fbaccess']['user_access_token'] = permanent_user_access_token
+		shaonutil.file.write_configuration_ini(config, 'config.ini')
+		print("permanent access_token achieved !")
---
+++ main.py
@@ -0,0 +1,103 @@
+#-*-coding:utf-8-*-
+from fb_utility import *
+
+
+def post(page_access_token,page_id):
+	fb = facebook.GraphAPI(page_access_token)
+	try:
+		fb.put_object(page_id,'feed',message = message_s)
+	except facebook.GraphAPIError:
+		raise ValueError("Go to https://developers.facebook.com/tools/explorer/v2 to get access token")
+
+def get_info(page_access_token,page_id,fields='id'):
+	# fields = 'id,country_page_likes,likes,new_like_count,fan_count'
+	fb = facebook.GraphAPI(page_access_token)
+	default_info = fb.get_object(id=page_id,fields=fields)
+	print(default_info)
+
+def get_insights(page_access_token,page_id):
+	fb = facebook.GraphAPI(page_access_token)
+	#page_impressions = fb.get_connections(id=page_id,connection_name='insights',metric='page_impressions',date_preset='yesterday',period='lifetime',show_description_from_api_doc=True)
+	#print(page_impressions)
+	#posts_25 = fb.get_object(id=page_id, fields='posts.fields(type, name, created_time, object_id)')
+	#print(posts_25)
+	page_impressions = fb.get_connections(id=page_id,connection_name='insights',metric='page_impressions',date_preset='yesterday',period='lifetime',show_description_from_api_doc=True)
+	print(page_impressions)
+
+def get_post_reach(page_access_token,post_id):
+	fb = facebook.GraphAPI(page_access_token)
+	s_post = fb.get_object(id=post_id,fields='id,created_time,message,comments.summary(total_count),reactions.summary(total_count),shares')
+	print(s_post)
+
+
+
+
+    
+
+
+		
+	
+
+
+
+
+config = shaonutil.file.read_configuration_ini('private/config.ini')
+
+user_id = config['fbaccess']['user_id']
+#get from app
+client_id = config['fbaccess']['client_id']
+client_secret = config['fbaccess']['client_secret']
+page_access_token = config['fbaccess']['page_access_token']
+user_access_token = config['fbaccess']['user_access_token']
+page_id = config['fbaccess']['page_id']
+message_s = 'posted by test app'
+
+
+# get_access_token_via_dialogue(client_id,'pages_show_list,manage_pages,publish_pages')
+
+
+
+
+
+
+try:
+	routine_on_access_error_get_permanent_token(client_id,client_secret,'pages_show_list,manage_pages,publish_pages')
+	get_post_reach(user_access_token,'105374151053772_108251464099374')
+except Exception as e:
+    s = str(e)
+    print(s)
+    if 'Error validating access token' in s:
+    	routine_on_access_error_get_permanent_token(client_id,client_secret,'pages_show_list,manage_pages,publish_pages')
+
+# {'error': {'message': 'Error validating access token: The session was invalidated previously using an API call.', 'type': 'OAuthException', 'code': 190, 'error_subcode': 463, 'fbtrace_id': 'ALveHTVATc4kDV-X7je8jpR'}}
+
+"""
+runable = True
+for pname in ['manage_pages','publish_pages']:
+	if pname not in debug_data['data']['scopes']:
+		runable = False
+
+if(runable):
+	#get_info(page_access_token,page_id,fields = 'id,feed')
+	get_post_reach(permanent_user_access_token,'105374151053772_108251464099374')
+"""
+
+
+"""
+Output for analyzing
+{
+	'id': '105374151053772_108251464099374', 
+	'created_time': '2020-02-20T06:05:28+0000', 
+	'message': 'Second Test Post by app', 
+	'comments': {'data': [], 'summary': {'total_count': 0}}, 
+	'reactions': {
+		'data': [
+			{'id': '780552288661036', 'name': 'Robist', 'type': 'LIKE'},
+ 			{'id': '2511694345603044', 'name': 'Shaon Majumder', 'type': 'LIKE'}, 
+ 			{'id': '105374151053772', 'name': 'Business Test Page', 'type': 'LIKE'}
+ 		],
+  		'paging': {'cursors': {'before': 'QVFIUnpuendLM2Q2STE2UEFJd3hFRVg3SXdUbGc5THZAqTEJ0dDZAQN3RBbE1ZAVUdHNi0wSm1IYW1sQ0k4Q1oySml0NXRYZAGJTZATA0b24wUjJBV1lmRldZAM0JR', 'after': 'QVFIUlpnVHF3X3NyaHZA4NzEwNHA2YWl3RjlFOVBsM1JWbGg5VDZAGVTFWOHk5ZADA5NUw4RTBJdTRZAX1dHNURLRFl5TEpEckpTNXl1bWx6a1cxVlVETlh5b1h3'}},
+  		'summary': {'total_count': 3}
+  	}
+}
+"""
